5d662a111e463a1fc79fc5da9c3e5522239aaf6c
JuanEstebanGarciaV <juan.garcia@epayco.com>
2025-11-19 17:10:07 -0500

    feat: agregar nuevo checkout

--- epayco/epayco.php
+++ epayco/epayco.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /**
@@ -12,6 +13,7 @@
 // No direct access
 defined('_JEXEC') or die('Restricted access');
 JHtml::_('script', 'https://checkout.epayco.co/checkout.js');
+// JHtml::_('script', 'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js');
 jimport('joomla.plugin.plugin');
 $lang = JFactory::getLanguage();
 
@@ -35,8 +37,17 @@ class PlgPaymentEpayco extends JPlugin
 	{
 		parent::__construct($subject, $config);
 
+		// Inicializar la propiedad params correctamente
+		if (isset($config) && isset($config->params)) {
+			$this->params = $config->params;
+		} elseif (isset($this->params)) {
+			// Ya está inicializada
+		} else {
+			$this->params = new stdClass();
+		}
+
 		// Set the language in the class
-		$config = JFactory::getConfig();
+		$configJ = JFactory::getConfig();
 
 		// Define Payment Status codes in Paypal  And Respective Alias in Framework
 		$this->responseStatus = array(
@@ -202,6 +213,8 @@ class PlgPaymentEpayco extends JPlugin
 		return $html;
 	}
 
+
+	
 	/**
 	 * onTP_ProcessSubmit
 	 *
@@ -269,4 +282,172 @@ class PlgPaymentEpayco extends JPlugin
 			$plgPaymentHelper->Storelog($this->_name, $data);
 		}
 	}
+
+
+
+	public function epaycoBearerToken()
+	{
+		$publicKey = $this->params->get('epayco_public_key', '');
+		$privateKey = $this->params->get('epayco_private_key', '');
+
+		if (!isset($_COOKIE[$publicKey])) {
+			$token = base64_encode($publicKey . ":" . $privateKey);
+			$bearer_token = $token;
+			setcookie($publicKey, $bearer_token, time() + (60 * 14), "/");
+		} else {
+			$bearer_token = $_COOKIE[$publicKey];
+		}
+
+		error_log('BearerToken debug: ' . print_r($bearer_token, true));
+
+		$headers = array(
+			'Content-Type' => 'application/json',
+			'Authorization' => "Basic " . $bearer_token
+		);
+		$response = $this->epayco_realizar_llamada_api("login", [], $headers);
+		error_log('Epayco API response: ' . print_r($response, true));
+		if ($response && isset($response['token'])) {
+			return $response['token'];
+		}
+		return null;
+	}
+
+	public function epayco_realizar_llamada_api($path, $data, $headers, $method = 'POST')
+	{
+		$url = 'https://eks-apify-service.epayco.io/' . $path;
+		// $url = 'https://apify.epayco.co/' . $path;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+		if (!empty($data)) {
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+		}
+		if (!empty($headers)) {
+			$formatted_headers = array();
+			foreach ($headers as $key => $value) {
+				$formatted_headers[] = $key . ': ' . $value;
+			}
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $formatted_headers);
+		}
+		$response_body = curl_exec($ch);
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		$error = curl_error($ch);
+		curl_close($ch);
+		if ($error) {
+			error_log("Error al hacer la llamada a la API de ePayco: " . $error);
+			return false;
+		}
+		if ($status_code == 200) {
+			$responseTransaction = json_decode($response_body, true);
+			return $responseTransaction;
+		} else {
+			error_log("Error en la respuesta de la API de ePayco, código de estado: " . $status_code);
+			return false;
+		}
+	}
+
+	/**
+	 * Construye la data para el checkout de ePayco
+	 * @param object $vars
+	 * @return array
+	 */
+	public function getCheckoutData($vars)
+	{
+		return array(
+			'checkout_version' => '2',
+			'name' => isset($vars->user_firstname) ? $vars->user_firstname . ' ' . $vars->user_lastname : 'Shops Online S.A.S',
+			'description' => isset($vars->descripcion) ? $vars->descripcion : 'Descripción del producto',
+			'currency' => isset($vars->currency_code) ? $vars->currency_code : 'COP',
+			'amount' => isset($vars->amount) ? floatval($vars->amount) : 20000.00,
+			'lang' => isset($vars->lang) ? $vars->lang : 'es',
+			'ip' => '179.12.113.12',
+			'test' => (isset($vars->test) && ($vars->test === true || $vars->test === 'true' || $vars->test === 1 || $vars->test === '1')) ? true : false,
+			'country' => isset($vars->country) ? $vars->country : 'CO',
+			'taxBase' => isset($vars->tax_base) ? floatval($vars->tax_base) : 16806.72,
+			'tax' => isset($vars->tax) ? floatval($vars->tax) : 3193.28,
+			'taxIco' => 0,
+			'response' => isset($vars->return) ? $vars->return : 'https://mysite.com',
+			'confirmation' => isset($vars->confirmUrl) ? $vars->confirmUrl : 'https://webhook.site/8b4bb363-099e-42e8-afe7-0bf11c59eeb1',
+			'methodsDisable' => array(),
+			'method' => 'POST',
+			'dues' => 1,
+			'extras' => array(
+				'extra1' => 'extra1',
+				'extra2' => isset($vars->orderId) ? $vars->orderId : 'extra2',
+				'extra3' => 'extra3',
+				'extra4' => 'extra4',
+				'extra5' => 'P33',
+				'extra6' => 'extra6',
+				'extra7' => 'extra7',
+				'extra8' => 'extra8',
+				'extra9' => 'extra9',
+				'extra10' => 'extra10',
+				'extra11' => 'extra11'
+			),
+			'billing' => array(
+				'email' => isset($vars->user_email) ? $vars->user_email : 'cliente@gmail.com',
+				'name' => isset($vars->user_firstname) ? $vars->user_firstname . ' ' . $vars->user_lastname : 'Cliente Martinez',
+				'address' => isset($vars->user_address) ? $vars->user_address : 'AV 18 # 18 - 17',
+				'typeDoc' => 'CC',
+				'numberDoc' => isset($vars->user_document) ? $vars->user_document : '103242123',
+				'callingCode' => '+57',
+				'mobilePhone' => isset($vars->user_phone) ? $vars->user_phone : '312456654'
+			)
+		);
+	}
+
+		/**
+		 * Realiza la petición a la API de ePayco para crear la sesión y retorna el sessionId
+		 * @param object $vars
+		 * @return string|null sessionId
+		 */
+		public function SessionId($vars)
+		{
+			$url = 'https://eks-apify-service.epayco.io/payment/session/create';
+
+			$bearerToken = $this->epaycoBearerToken();
+			// Headers deben ser array plano tipo "Header: value"
+			$headers = array(
+				'Content-Type: application/json',
+				'Authorization: Bearer ' . $bearerToken
+			);
+
+			error_log('Headers enviados a ePayco: ' . print_r($headers, true));
+			$data = $this->getCheckoutData($vars);
+			error_log('Data enviada a ePayco: ' . print_r($data, true));
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+			curl_setopt($ch, CURLOPT_POST, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			$response_body = curl_exec($ch);
+			error_log('SessionId API raw response: ' . print_r($response_body, true));
+			$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			$error = curl_error($ch);
+			curl_close($ch);
+
+			if ($error) {
+				error_log("Error al crear la sesión de ePayco: " . $error);
+				return null;
+			}
+
+			if ($status_code == 200) {
+				$response = json_decode($response_body, true);
+				error_log('Respuesta decodificada de ePayco: ' . print_r($response, true));
+				if (isset($response['success']) && $response['success'] && isset($response['data']['sessionId'])) {
+					return $response['data']['sessionId'];
+				} else {
+					error_log("Respuesta inesperada al crear la sesión de ePayco: " . print_r($response, true));
+					return null;
+				}
+			} else {
+				error_log("Error en la respuesta de la API de ePayco al crear sesión, código de estado: " . $status_code);
+				return null;
+			}
+		}
 }
--- epayco/epayco/tmpl/checkout.php
+++ epayco/epayco/tmpl/checkout.php
@@ -12,107 +12,71 @@ defined('_JEXEC') or die('Restricted access');
 $order_id_explode = explode('=', $vars->notify_url);
 $order_id = substr($order_id_explode[3], 0, strpos($order_id_explode[3], "&processor"));
 
+
+$sessionId = $this->SessionId($vars);
+echo '<pre>SessionId debug: ' . var_export($sessionId, true) . '</pre>';
+if ($sessionId === null || $sessionId === '') {
+    echo '<pre style="color:red">No se obtuvo sessionId. Revisa los logs de PHP y la función SessionId.</pre>';
+}
+
+
+
+
 ?>
+
+<h1><?php echo $sessionId; ?></h1>
+
 <center>
-    <a id="btn_epayco" href="#">
-        <img src="https://multimedia-epayco.s3.amazonaws.com/plugins-sdks/Boton-color-espanol.png">
+
+    <a id="payBtn" href="#">
+        <img src="https://multimedia.epayco.co/epayco-landing/btns/Boton-epayco-color1.png">
     </a>
 </center>
-<form>
-    <script  src="https://checkout.epayco.co/checkout.js"></script>
-    <script>
-        var handler = ePayco.checkout.configure({
-            key: "<?php echo $vars->publicKey; ?>",
-            test: "<?php echo $vars->test; ?>".toString()
-        })
-        var extras_epayco = {
-            extra5: "P33"
+
+<!-- <script src="https://epayco-checkout-testing.s3.amazonaws.com/checkout.preprod-v2.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
+
+<script>
+    // Función que se ejecuta al hacer clic
+    async function handlePayment() {
+        console.log("🔄 Iniciando proceso de pago...");
+
+        // 1. Obtener sessionId del backend (generado en PHP)
+        const sessionId = "<?php echo $sessionId; ?>";
+
+        if (!sessionId || sessionId === "") {
+            alert("❌ No se pudo obtener el sessionId. Verifica la configuración.");
+            return;
         }
-        var data = {
-            name: "<?php echo "Order # " . $vars->orderId; ?>",
-            description: "<?php echo $vars->descripcion; ?>",
-            invoice: "<?php echo "Order # " . $vars->orderId; ?>",
-            currency: "<?php echo $vars->currency_code; ?>",
-            amount: "<?php echo sprintf('%02.2f', $vars->amount) ?>".toString(),
-            tax_base: "<?php echo sprintf('%02.2f', $vars->tax_base) ?>".toString(),
-            tax: "<?php echo sprintf('%02.2f', $vars->tax) ?>".toString(),
-            taxIco: "0".toString(),
-            country: "CO",
-            lang: "es",
-            extra2: "<?php echo $vars->orderId; ?>",
-            external: "<?php echo $vars->external; ?>",
-            confirmation: "<?php echo $vars->confirmUrl; ?>",
-            response: "<?php echo $vars->return; ?>",
-            name_billing: "<?php echo $vars->user_firstname . " " . $vars->user_lastname; ?>",
-            address_billing: "",
-            email_billing: "<?php echo $vars->user_email; ?>",
-            autoclick: "true",
-            ip: "<?php echo $vars->ip; ?>",
-            test: "<?php echo $vars->test; ?>".toString()
-        }
-        const apiKey = "<?php echo $vars->publicKey; ?>";
-        const privateKey = "<?php echo $vars->privateKey; ?>";;
-        var openChekout = function() {
-            if (localStorage.getItem("invoicePayment") == null) {
-                localStorage.setItem("invoicePayment", data.invoice);
-                makePayment(privateKey, apiKey, data, data.external == "true" ? true : false)
-            } else {
-                if (localStorage.getItem("invoicePayment") != data.invoice) {
-                    localStorage.removeItem("invoicePayment");
-                    localStorage.setItem("invoicePayment", data.invoice);
-                    makePayment(privateKey, apiKey, data, data.external == "true" ? true : false)
-                } else {
-                    makePayment(privateKey, apiKey, data, data.external == "true" ? true : false)
-                }
-            }
-        }
-        var makePayment = function(privatekey, apikey, info, external) {
-            const headers = {
-                "Content-Type": "application/json"
-            };
-            headers["privatekey"] = privatekey;
-            headers["apikey"] = apikey;
-            var payment = function() {
-                return fetch("https://cms.epayco.co/checkout/payment/session", {
-                        method: "POST",
-                        body: JSON.stringify(info),
-                        headers
-                    })
-                    .then(res => res.json())
-                    .catch(err => err);
-            }
-            payment()
-                .then(session => {
-                    if (session.data.sessionId != undefined) {
-                        localStorage.removeItem("sessionPayment");
-                        localStorage.setItem("sessionPayment", session.data.sessionId);
-                        const handlerNew = window.ePayco.checkout.configure({
-                            sessionId: session.data.sessionId,
-                            external: external,
-                        });
-                        handlerNew.openNew()
-                    } else {
-                        handler.open(data)
-                    }
-                })
-                .catch(error => {
-                    error.message;
-                });
-        }
-        var bntPagar = document.getElementById("btn_epayco");
-        bntPagar.addEventListener("click", openChekout);
-        openChekout()
-        jQuery(document).ready(function($) {
-            document.addEventListener("contextmenu", function(e) {
-                e.preventDefault();
-            }, false);
-        })
-        jQuery(document).keydown(function(event) {
-            if (event.keyCode == 123) {
-                return false;
-            } else if (event.ctrlKey && event.shiftKey && event.keyCode == 73) {
-                return false;
-            }
+
+        console.log("✅ SessionId obtenido:", sessionId);
+
+        // 2. Configurar checkout
+        const checkout = window.ePayco.checkout.configure({
+            sessionId: sessionId,
+            type: "onepage",
+            test: true
         });
-    </script>
-</form>
+
+        // 3. Eventos
+        checkout.onCreated(() => console.log("✅ Checkout creado"));
+        checkout.onErrors(e => console.error("❌ Error:", e));
+        checkout.onClosed(() => console.log("🔒 Checkout cerrado"));
+
+        // 4. Abrir checkout
+        checkout.open();
+    }
+
+    var bntPagar = document.getElementById("payBtn");
+
+    // Asignar evento al botón
+    document.addEventListener('DOMContentLoaded', () => {
+        // document.getElementById("payBtn").onclick = handlePayment;
+        console.log("Archivo JS cargado correctamente...");
+        bntPagar.addEventListener('click', function(event) {
+            // . Evita que el navegador realice la acción por defecto del enlace (navegar)
+            event.preventDefault();
+
+        });
+    });
+</script>
